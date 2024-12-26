d86c7f05ca3ea1ad424fe68405a43b23fac8e4ed
VitoHendriansyah <vitohendriansyah28@gmail.com>
2024-12-26 15:16:56 +0700

    Update projectalgro.cpp

--- projectalgro.cpp
+++ projectalgro.cpp
@@ -6,13 +6,8 @@ void garis(){
     cout << "========================================================" << endl;
 }
 int main(){
-<<<<<<< HEAD
     cout << "Selamt Datang Di Rental Cahaya Abadi" << endl;
-
-
-=======
     cout << "Hallo WOrld" << endl;
     cout << "Hai" << endl;
     cout << "Tolong" << endl;
->>>>>>> e53cc0de6805dd58df0ce3dd643c2742a2360949
 }
